aa6f90d0ef68080de44689d8ba7b98e3e4a9e24c
krishna2804 <pamuri1997@gmail.com>
2018-06-16 14:34:08 +0530

    added constructor

---
+++ project1/src/emailapp/Email.java
@@ -0,0 +1,31 @@
+package emailapp;
+
+public class Email {
+	private String first_name;
+	private String last_name;
+	private String password;
+	private String department;
+	private int mailbox_Capacity;
+	private String alternate_mail;
+	
+	//firstname and lastname
+	public Email(String firstname,String lastname)
+	{
+		first_name=firstname;
+		last_name=lastname;
+		System.out.println("Names:"+first_name+last_name);
+	}
+	
+	
+	
+	//Ask for department
+	
+	//generate password
+	
+	//set the mailboxcapacity
+	
+	//set alternate mail
+	
+	//update password
+
+}
---
+++ project1/src/emailapp/EmailApp1.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class EmailApp1 {
+	
+	public static void main(String[] args) 
+	{
+		Email mail=new Email("king","raju");
+	}
+	
+
+}
