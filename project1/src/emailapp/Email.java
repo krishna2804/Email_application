658b65d5b2ec7fa890b569c009011d5ab9f7304b
krishna2804 <pamuri1997@gmail.com>
2018-06-16 21:16:59 +0530

    added new methods to update password and alternate mail

--- project1/src/emailapp/Email.java
+++ project1/src/emailapp/Email.java
@@ -8,7 +8,7 @@ public class Email {
 	private String password;
 	private int length=10;
 	private String department;
-	private int mailbox_Capacity;
+	private int mailbox_Capacity=500;
 	private String alternate_mail;
 	private String companysuffix=".company.com";
 	private String email;
@@ -18,18 +18,18 @@ public class Email {
 	{
 		first_name=firstname;
 		last_name=lastname;
-		System.out.println("Names:"+first_name+last_name);
+		//System.out.println("Names:"+first_name+last_name);
 		
 		department=setDepartment();
-		System.out.println("Department name:"+department);
+		//System.out.println("Department name:"+department);
 		
 		password=setPassword(length);
-		System.out.println("password name:"+password);
+		//System.out.println("password name:"+password);
 		
 		// creating email
 		email=first_name.toLowerCase()+"."+last_name.toLowerCase()+"@"+department+companysuffix;
 		
-		System.out.print(email);
+		//System.out.print(email);
 
 		
 	}
@@ -38,8 +38,10 @@ public class Email {
 	private String setDepartment()
 	{
 		System.out.println("Please select your department:");
-		System.out.print("1-student\n2-faculty\n3-administration\n4-guest");
+		System.out.print("1-student\n2-faculty\n3-administration\n4-guest\n");
 		Scanner input=new Scanner(System.in);
+		System.out.print("Please select your department id-:");
+
 		int dep_in=input.nextInt();
 		if (dep_in==1) {
 			return "Student";
@@ -85,9 +87,35 @@ public class Email {
 	
 	
 	//set the mailboxcapacity
+	public void setmailboxcapacity(int capacity) {
+		mailbox_Capacity=capacity;
+	}
+	
 	
 	//set alternate mail
-	
+	public void setalternatemail(String newmail) {
+		alternate_mail=newmail;
+	}
 	//update password
+	public void setnewpassword(String newpass) {
+		password=newpass;
+	}
 
+	public int getmailboxcapacity() {
+		return mailbox_Capacity;
+	}
+	
+	public String getalternatemail() {
+		return alternate_mail;
+	}
+	
+	public String getnewpassword() {
+		return password;
+	}
+	
+	public String profile() {
+		System.out.println("************YOUR PROFILE****************");
+
+		return "Diplay name:"+first_name+" "+last_name+"\nCOMPANY MAIL:"+email+"\nMAILBOX CAPACITY:"+mailbox_Capacity+"mb";
+	}
 }
--- project1/src/emailapp/EmailApp1.java
+++ project1/src/emailapp/EmailApp1.java
@@ -5,6 +5,13 @@ public class EmailApp1 {
 	public static void main(String[] args) 
 	{
 		Email mail=new Email("king","raju");
+		mail.setalternatemail("kkkrt.com");
+		String newmail=mail.getalternatemail();
+		//System.out.println(newmail);
+		
+		String data=mail.profile();
+		System.out.println(data);
+		
 	}
 	
 
