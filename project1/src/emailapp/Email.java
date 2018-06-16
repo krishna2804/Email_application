9af5606781cc19635d71f444cf7a4a9b50c826f0
krishna2804 <pamuri1997@gmail.com>
2018-06-16 14:54:11 +0530

    Added setdepartment method to get user department

--- project1/src/emailapp/Email.java
+++ project1/src/emailapp/Email.java
@@ -1,4 +1,6 @@
 package emailapp;
+import java.util.*;
+import java.util.Scanner;
 
 public class Email {
 	private String first_name;
@@ -14,11 +16,35 @@ public class Email {
 		first_name=firstname;
 		last_name=lastname;
 		System.out.println("Names:"+first_name+last_name);
+		department=setDepartment();
+		System.out.println("Department name:"+department);
 	}
 	
-	
-	
 	//Ask for department
+	private String setDepartment() 
+	{
+		System.out.println("Please select your department:");
+		System.out.print("1-student\n2-faculty\n3-administration\n4-guest");
+		Scanner input=new Scanner(System.in);
+		int dep_in=input.nextInt();
+		if (dep_in==1) {
+			return "Student";
+		}
+		else if (dep_in==2) {
+			return "faculty";
+		}
+		else if (dep_in==3) {
+			return "administration";
+		}
+		else if (dep_in==4) {
+			return "guest";
+		}
+		else {
+			return "";
+		}
+		
+		
+	}
 	
 	//generate password
 	
